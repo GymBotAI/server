a2d0b40eb99a3e019b0c2fc1cc92e4c3b69def8a
lafkpages <soy.lafk+gh@gmail.com>
2023-09-13 19:32:48 +0200

    Upgrade to OpenAI v4

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import _basePrompt from "./prompt.json" assert { type: "json" };
 
-import { Configuration as OpenAIConfig, OpenAIApi } from "openai";
-import type { ChatCompletionRequestMessage } from "openai";
+import OpenAI from "openai";
+type ChatCompletionMessage = Parameters<
+  typeof OpenAI.Chat.Completions.prototype.create
+>[0]["messages"][number];
 
 const basePrompt = _basePrompt as {
-  messages: ChatCompletionRequestMessage[];
+  messages: ChatCompletionMessage[];
 };
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -14,11 +16,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const isDevelopment = process.env.NODE_ENV != "production";
 
 const openaiChatModel = "gpt-3.5-turbo";
-const openai = new OpenAIApi(
-  new OpenAIConfig({
-    apiKey: process.env.OPENAI_KEY,
-  })
-);
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_KEY,
+});
 
 const streamEndToken = "[DONE]";
 
@@ -35,170 +35,124 @@ type WebSocketData = {
    * The messages that have been sent
    * between the client and the server
    */
-  messages: ChatCompletionRequestMessage[];
+  messages: ChatCompletionMessage[];
 };
 
-Bun.serve<WebSocketData>(
-  {
-    development: isDevelopment,
-    port: process.env.PORT || "3001",
-    fetch(req, server) {
-      // upgrade the request to a WebSocket
-      if (
-        server.upgrade(req, {
-          data: {
-            authed: false,
-            messages: [],
-          },
-        })
-      ) {
+Bun.serve<WebSocketData>({
+  development: isDevelopment,
+  port: process.env.PORT || "3001",
+  fetch(req, server) {
+    // upgrade the request to a WebSocket
+    if (
+      server.upgrade(req, {
+        data: {
+          authed: false,
+          messages: [],
+        },
+      })
+    ) {
+      return;
+    }
+
+    return new Response("Upgrade failed :(", { status: 500 });
+  },
+  websocket: {
+    open(ws) {
+      console.debug(`[${ws.remoteAddress}]`, "WS client connected");
+    },
+    close(ws) {
+      console.debug(`[${ws.remoteAddress}]`, "WS client disconnected");
+    },
+    async message(ws, message) {
+      if (typeof message != "string") {
         return;
       }
 
-      return new Response("Upgrade failed :(", { status: 500 });
-    },
-    websocket: {
-      open(ws) {
-        console.debug(`[${ws.remoteAddress}]`, "WS client connected");
-      },
-      close(ws) {
-        console.debug(`[${ws.remoteAddress}]`, "WS client disconnected");
-      },
-      async message(ws, message) {
-        if (typeof message != "string") {
-          return;
+      // WebSocket auth
+      if (!ws.data.authed) {
+        if (chatSecret) {
+          ws.data.authed = message == chatSecret;
+        } else {
+          console.warn("REQ_SECRET env var not found, skipping auth");
+          ws.data.authed = true;
         }
 
-        // WebSocket auth
-        if (!ws.data.authed) {
-          if (chatSecret) {
-            ws.data.authed = message == chatSecret;
-          } else {
-            console.warn("REQ_SECRET env var not found, skipping auth");
-            ws.data.authed = true;
-          }
-
-          if (ws.data.authed) {
-            console.debug(`[${ws.remoteAddress}]`, "WS client authenticated");
-          }
-
-          return;
+        if (ws.data.authed) {
+          console.debug(`[${ws.remoteAddress}]`, "WS client authenticated");
         }
 
-        console.debug(`[${ws.remoteAddress}]`, "WS message:", message);
+        return;
+      }
 
-        // Demo messages in development
-        if (isDevelopment) {
-          // ws.send("Hello, demo response message!");
-          // ws.send(streamEndToken);
-          //
-          // let intv = 0;
-          // intv = setInterval(() => {
-          //   ws.send('aa\n');
-          // }, 50);
-          // setTimeout(() => {
-          //   clearInterval(intv);
-          //   ws.send(streamEndToken);
-          // }, 5000);
+      console.debug(`[${ws.remoteAddress}]`, "WS message:", message);
 
-          ws.send("This is a paragraph");
-          ws.send(streamEndToken);
-          ws.send("This is another");
-          ws.send(streamEndToken);
-          return;
-        }
+      // Demo messages in development
+      if (isDevelopment) {
+        // ws.send("Hello, demo response message!");
+        // ws.send(streamEndToken);
+        //
+        // let intv = 0;
+        // intv = setInterval(() => {
+        //   ws.send('aa\n');
+        // }, 50);
+        // setTimeout(() => {
+        //   clearInterval(intv);
+        //   ws.send(streamEndToken);
+        // }, 5000);
 
-        ws.data.messages.push({
-          role: "user",
-          content: message,
-        
+        ws.send("This is a paragraph");
+        ws.send(streamEndToken);
+        ws.send("This is another");
+        ws.send(streamEndToken);
+        return;
+      }
+
+      ws.data.messages.push({
+        role: "user",
+        content: message,
+      });
+
+      try {
+        const chatCompletion = await openai.chat.completions.create({
+          model: openaiChatModel,
+          messages: ws.data.messages,
+          stream: true,
         });
 
-        try {
-          const chatCompletion = await openai.createChatCompletion(
-            {
-              model: openaiChatModel,
-              messages: ws.data.messages,
-              stream: true,
-            },
-            {
-              responseType: "stream",
-            }
-          );
-  
-          ws.data.messages.push({
-            role: "assistant",
-            content: "",
-          });
-  
-          chatCompletion.data.on("data", (raw) => {
-            const chunks = raw
-              .toString()
-              .split("\n\n")
-              .map((s) => s.replace(/^data: /, ""));
-  
-            let finalChunk = "";
-            let didEnd = false;
-  
-            for (const chunk of chunks) {
-              if (!chunk) {
-                continue;
-              }
-  
-              if (chunk == streamEndToken) {
-                didEnd = true;
-                break;
-              }
-  
-              let data = null;
-              try {
-                data = JSON.parse(chunk);
-              } catch (err) {
-                console.error(
-                  "Error parsing chunk:",
-                  err,
-                  "Chunk is",
-                  JSON.stringify(chunk)
-                );
-                return;
-              }
-  
-              const chunkContent = data.choices[0]?.delta?.content;
-  
-              if (!chunkContent) {
-                continue;
-              }
-  
-              finalChunk += chunkContent;
-            }
-  
-            ws.data.messages[ws.data.messages.length - 1].content += finalChunk;
-  
-            // Split message newlines into different messages
-            // A.K.A. chunking
-            const lines = finalChunk.split(/(?:\r?\n){1,2}/);
-            for (let i in lines) {
-              ws.send(lines[i]);
-              if (i < lines.length - 1) {
-                ws.send(streamEndToken);
-              }
-            }
-  
-            if (didEnd) {
+        ws.data.messages.push({
+          role: "assistant",
+          content: "",
+        });
+
+        for await (const chunk of chatCompletion) {
+          const chunkContent = chunk.choices[0].delta.content;
+
+          if (typeof chunkContent != "string") {
+            continue;
+          }
+
+          ws.data.messages[ws.data.messages.length - 1].content += chunkContent;
+
+          // Split message newlines into different messages
+          // A.K.A. chunking
+          const lines = chunkContent.split(/(?:\r?\n){1,2}/);
+          for (let _i in lines) {
+            const i = parseInt(_i);
+
+            ws.send(lines[i]);
+            if (i < lines.length - 1) {
               ws.send(streamEndToken);
             }
-          });
-  
-          chatCompletion.data.on("end", () => {
-            ws.send(streamEndToken);
-          });
-        } catch (err) {
-          console.error("Error in chatCompletion:", err);
+          }
         }
-      },
+
+        ws.send(streamEndToken);
+      } catch (err) {
+        console.error("Error in chatCompletion:", err);
+      }
     },
-  }
-);
+  },
+});
 
 if (isDevelopment) {
   console.warn("In development mode!");
