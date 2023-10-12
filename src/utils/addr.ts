b3e3aaacd079468e15537acc0a5a509820e52a48
lafkpages <soy.lafk+gh@gmail.com>
2023-10-12 14:42:33 +0200

    getServerAddress lowercase

--- src/utils/addr.ts
+++ src/utils/addr.ts
@@ -4,7 +4,7 @@ import type { Server } from "bun";
 
 export function getServerAddress(server: Server) {
   if (process.env.REPL_HOME) {
-    return `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`;
+    return `https://${process.env.REPL_SLUG?.toLowerCase()}.${process.env.REPL_OWNER?.toLowerCase()}.repl.co`;
   } else if (localIp) {
     return `http://${localIp}:${server.port}`;
   } else {
