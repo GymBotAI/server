c693d867b22a3439a084cce22ef60807cf18333a
lafkpages <soy.lafk+gh@gmail.com>
2023-10-12 14:38:01 +0200

    Fix addr util using wrong Replit env vars

--- src/utils/addr.ts
+++ src/utils/addr.ts
@@ -1,7 +1,7 @@
 import type { Server } from "bun";
 
 export function getServerAddress(localIp: string | null, server: Server) {
-  if (process.env.REPLIT_HOME) {
+  if (process.env.REPL_HOME) {
     return `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`;
   } else if (localIp) {
     return `http://${localIp}:${server.port}`;
