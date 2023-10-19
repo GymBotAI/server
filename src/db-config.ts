9db80fb3d9db13d326bb25264d04ad31109dabbc
lafkpages <soy.lafk+gh@gmail.com>
2023-10-19 20:42:33 +0200

    Remove DB config module

--- src/db-config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  host: process.env.GYMBOT_SERVER_DB_HOST || "localhost",
-  database: "gymbot",
-  user: process.env.GYMBOT_SERVER_DB_USER || "admindb",
-  password: process.env.GYMBOT_SERVER_DB_PASSWORD || "Password",
-};
