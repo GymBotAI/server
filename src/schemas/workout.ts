1368e9c88c38566cdd491be71ebd11615350d0e6
lafkpages <luisafk@proton.me>
2023-11-09 16:01:02 +0100

    Workout API endpoint schema minimum duration 5mins

--- src/schemas/workout.ts
+++ src/schemas/workout.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export default z.object({
-  duration: z.number().positive(),
+  duration: z.number().min(5),
   bodypart: z.string().min(2),
   notes: z.union([z.null(), z.string()]),
 });
