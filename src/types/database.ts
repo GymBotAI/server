8e14b39e4c1c28fc3671b65d5244da319fe57d52
lafkpages <luisafk@proton.me>
2023-11-09 15:39:05 +0100

    Prettify

--- src/types/database.ts
+++ src/types/database.ts
@@ -4,57 +4,57 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export interface Database {
   public: {
     Tables: {
       users: {
         Row: {
-          birthday: string | null
-          gender: string | null
-          height: number | null
-          id: string
-          name: string | null
-          weight: number | null
-        }
+          birthday: string | null;
+          gender: string | null;
+          height: number | null;
+          id: string;
+          name: string | null;
+          weight: number | null;
+        };
         Insert: {
-          birthday?: string | null
-          gender?: string | null
-          height?: number | null
-          id: string
-          name?: string | null
-          weight?: number | null
-        }
+          birthday?: string | null;
+          gender?: string | null;
+          height?: number | null;
+          id: string;
+          name?: string | null;
+          weight?: number | null;
+        };
         Update: {
-          birthday?: string | null
-          gender?: string | null
-          height?: number | null
-          id?: string
-          name?: string | null
-          weight?: number | null
-        }
+          birthday?: string | null;
+          gender?: string | null;
+          height?: number | null;
+          id?: string;
+          name?: string | null;
+          weight?: number | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "users_id_fkey"
-            columns: ["id"]
-            referencedRelation: "users"
-            referencedColumns: ["id"]
+            foreignKeyName: "users_id_fkey";
+            columns: ["id"];
+            referencedRelation: "users";
+            referencedColumns: ["id"];
           }
-        ]
-      }
-    }
+        ];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
