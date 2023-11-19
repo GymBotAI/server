475770dd74036e52ed9df9be5a455fd1d0cb69db
lafkpages <luisafk@proton.me>
2023-11-19 21:04:33 +0100

    Return header indicating dev mode

--- src/routes/root/index.ts
+++ src/routes/root/index.ts
@@ -1,3 +1,9 @@
-export default function handler() {
-  return new Response("Hi");
+export default function handler(req: Request, env: Env, isDev: boolean) {
+  return new Response("Hi", {
+    headers: isDev
+      ? {
+          "x-gymbot-dev": "1",
+        }
+      : {},
+  });
 }
