d62e5e6a7f4eacfa3ca58e4d3c6f9c8de2a08c0a
lafkpages <luisafk@proton.me>
2023-11-20 09:50:27 +0100

    Chat WS check IP before Upgrade header

--- src/routes/chat/index.ts
+++ src/routes/chat/index.ts
@@ -1,15 +1,15 @@
 import WsHandler from "./ws";
 
 export default async function handler(req: Request, env: Env, isDev: boolean) {
-  const upgradeHeader = req.headers.get("Upgrade");
-  if (!upgradeHeader || upgradeHeader !== "websocket") {
-    return new Response("Expected Upgrade: websocket", { status: 426 });
-  }
-
   const ip = req.headers.get("cf-connecting-ip");
 
   if (!ip) {
     return new Response("Expected IP", { status: 500 });
+  }
+
+  const upgradeHeader = req.headers.get("Upgrade");
+  if (!upgradeHeader || upgradeHeader !== "websocket") {
+    return new Response("Expected Upgrade: websocket", { status: 426 });
   }
 
   const webSocketPair = new WebSocketPair();
